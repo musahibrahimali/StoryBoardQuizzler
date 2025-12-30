66eaae43d29f561935c0ddb7f50cf0c5650185ab
musah <nkwabyte@gmail.com>
2025-12-30 02:30:17 +0000

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Musah Ibrahim Ali on 12/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
